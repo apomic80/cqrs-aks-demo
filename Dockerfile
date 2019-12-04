dbc774d495d8a09720b9ca131f83bc61f49ccf07
apomic80 <michele.aponte@blexin.com>
2019-12-04 21:42:48 +0100

    demo1 done

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#step 2: build backend
+FROM mcr.microsoft.com/dotnet/core/sdk AS build
+RUN mkdir app
+WORKDIR /app
+COPY mycms .
+RUN dotnet restore
+RUN dotnet publish -c Release -o out
+
+#step 3: run
+FROM mcr.microsoft.com/dotnet/core/aspnet
+ENV SQLSERVER_CONNECTIONSTRING='Server=localhost;Database=mycms;User=sa;Password={password};MultipleActiveResultSets=true;'
+WORKDIR /app
+COPY --from=build /app/out .
+ENTRYPOINT dotnet mycms.dll
---
+++ mycms/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using mycms.Data.Entities;
+using mycms.Data.Infrastructure;
+using mycms.Models;
+using mycms.Models.ViewModels.Home;
+
+namespace mycms.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IModelReader<Article> reader = null;
+
+        public HomeController(
+            IModelReader<Article> reader)
+        {
+            this.reader = reader;
+        }
+
+        public IActionResult Index()
+        {
+            var result = this.reader
+                .Select(x => new ArticleItemViewModel(){
+                    Id = x.Id,
+                    Title = x.Title,
+                    Subtitle = x.Subtitle,
+                    Author = x.Author
+                }).ToList();
+
+            return View(result);
+        }
+        public IActionResult Article(int id)
+        {
+            var result = this.reader
+                .Where(x => x.Id == id)
+                .Select(x => new ArticleViewModel(){
+                    Id = x.Id,
+                    Title = x.Title,
+                    Subtitle = x.Subtitle,
+                    Content = x.Content,
+                    Author = x.Author
+                }).SingleOrDefault();
+
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
