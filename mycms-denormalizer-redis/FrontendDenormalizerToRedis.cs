ebc4f605941055c124c5b977f3c228a733a07693
apomic80 <michele.aponte@blexin.com>
2019-12-05 10:05:29 +0100

    wip

---
+++ mycms-denormalizer-redis/FrontendDenormalizerToRedis.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using mycms_shared.Entities;
+using mycms_shared.Events;
+using mycms_shared.Infrastructure;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StackExchange.Redis;
+
+namespace mycms_denormalizer_redis
+{
+    public class FrontendDenormalizerToRedis : BackgroundService
+    {
+        private readonly ILogger logger;
+        private IConnection connection;
+        private IModel channel;
+        private IDatabase cache;
+        private string queueName;
+
+        public FrontendDenormalizerToRedis(ILoggerFactory loggerFactory)
+        {
+            this.logger = loggerFactory.CreateLogger<FrontendDenormalizerToRedis>();
+            this.initRabbitMQ();
+        }
+
+        private void initRedis()
+        {
+            var redisConnString = Environment.GetEnvironmentVariable("REDIS_CONNECTIONSTRING");
+            var password = Environment.GetEnvironmentVariable("REDIS_PASSWORD");
+            redisConnString = redisConnString.Replace("{password}", password);
+            IDatabase cache = ConnectionMultiplexer.Connect(redisConnString).GetDatabase();
+        }
+
+        private void initRabbitMQ()
+        {
+            var hostName = Environment.GetEnvironmentVariable("RABBIT_HOSTNAME");
+            var userName = Environment.GetEnvironmentVariable("RABBIT_USER");
+            var password = Environment.GetEnvironmentVariable("RABBIT_PASSWORD");
+            var port = Environment.GetEnvironmentVariable("RABBIT_PORT");
+            queueName = Environment.GetEnvironmentVariable("FRONTEND_QUEUE_NAME");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = hostName,
+                UserName = userName,
+                Password = password,
+                Port = int.Parse(port)
+            };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "mycms", type: ExchangeType.Fanout);
+            channel.QueueBind(queue: queueName, exchange: "mycms", routingKey: "");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Process(message);
+                Console.WriteLine(" [x] {0}", message);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private void Process(string message)
+        {
+            var articleEvent = JsonSerializer.Deserialize<ArticleCRUDEvent>(message);
+            var json = cache.StringGet(typeof(Article).Name);
+            var articles = JsonSerializer.Deserialize<IList<Article>>(json);
+
+            switch (articleEvent.Operation)
+            {
+                case CRUDOperation.CREATE:
+                    {
+                        articles.Add(articleEvent.Entity);
+                    }
+                    break;
+
+                case CRUDOperation.UPDATE:
+                    {
+                        var article = articles.FirstOrDefault(x => x.Id == articleEvent.Entity.Id);
+                        if (article != null)
+                        {
+                            article.Title = articleEvent.Entity.Title;
+                            article.Subtitle = articleEvent.Entity.Subtitle;
+                            article.Content = articleEvent.Entity.Content;
+                            article.Author = articleEvent.Entity.Author;
+                        }
+                    }
+                    break;
+
+                case CRUDOperation.DELETE:
+                    {
+                        var article = articles.FirstOrDefault(x => x.Id == articleEvent.Entity.Id);
+                        if (article != null)
+                        {
+                            articles.Remove(article);
+                        }
+                    }
+                    break;
+            }
+
+            var articlesJson = JsonSerializer.Serialize(articles);
+            cache.StringSet(typeof(Article).Name, articlesJson);
+        }
+    }
+}
--- mycms-denormalizer-redis/Program.cs
+++ mycms-denormalizer-redis/Program.cs
@@ -1,107 +1,26 @@
-﻿using System;
-using System.Linq;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
-using System.Text.Json;
-using mycms_shared.Events;
-using mycms_shared.Infrastructure;
-using StackExchange.Redis;
-using mycms_shared.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace mycms_denormalizer_redis
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            var factory = new ConnectionFactory()
-            {
-                HostName = "localhost", // Environment.GetEnvironmentVariable("RABBIT_HOSTNAME"),
-                UserName = "user", // Environment.GetEnvironmentVariable("RABBIT_USER"),
-                Password = "jxJ1R6SECq", // Environment.GetEnvironmentVariable("RABBIT_PASSWORD"),
-                Port = 5672 //int.Parse(Environment.GetEnvironmentVariable("RABBIT_PORT"))
-            };
+            CreateHostBuilder(args).Build().Run();
+        }
 
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    channel.ExchangeDeclare(exchange: "mycms", type: ExchangeType.Fanout);
-
-                    var queueName = "frontend"; //Environment.GetEnvironmentVariable("FRONTEND_QUEUE_NAME");
-                    channel.QueueBind(queue: queueName,
-                                      exchange: "mycms",
-                                      routingKey: "");
-
-                    Console.WriteLine(" [*] Waiting for messages.");
-
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body);
-                        Process(message);
-                        Console.WriteLine(" [x] {0}", message);
-                    };
-                    channel.BasicConsume(queue: queueName,
-                                         autoAck: true,
-                                         consumer: consumer);
-
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
-            }
-        }
-
-        public static void Process(string message)
-        {
-
-            var articleEvent = JsonSerializer.Deserialize<ArticleCRUDEvent>(message);
-
-            var redisConnString = Environment.GetEnvironmentVariable("REDIS_CONNECTIONSTRING");
-            var password = Environment.GetEnvironmentVariable("REDIS_PASSWORD");
-            redisConnString = "localhost:6379,password=BUTzkID19Y,ssl=False,abortConnect=False"; // redisConnString.Replace("{password}", password);
-            IDatabase cache = ConnectionMultiplexer.Connect(redisConnString).GetDatabase();
-
-            var json = cache.StringGet(typeof(Article).Name);
-            var articles = JsonSerializer.Deserialize<IList<Article>>(json);
-
-            switch (articleEvent.Operation)
-            {
-                case CRUDOperation.CREATE:
-                    {
-                        articles.Add(articleEvent.Entity);
-                    }
-                    break;
-
-                case CRUDOperation.UPDATE:
-                    {
-                        var article = articles.FirstOrDefault(x => x.Id == articleEvent.Entity.Id);
-                        if (article != null)
-                        {
-                            article.Title = articleEvent.Entity.Title;
-                            article.Subtitle = articleEvent.Entity.Subtitle;
-                            article.Content = articleEvent.Entity.Content;
-                            article.Author = articleEvent.Entity.Author;
-                        }
-                    }
-                    break;
-
-                case CRUDOperation.DELETE:
-                    {
-                        var article = articles.FirstOrDefault(x => x.Id == articleEvent.Entity.Id);
-                        if (article != null)
-                        {
-                            articles.Remove(article);
-                        }
-                    }
-                    break;
-            }
-
-            var articlesJson = JsonSerializer.Serialize(articles);
-            cache.StringSet(typeof(Article).Name, articlesJson);
-        }
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
